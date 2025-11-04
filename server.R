17f9480be06fb240e59b24e6aee357a4b20e4b94
teranni <heszteranna@gmail.com>
2025-11-04 11:04:25 +0000

    Shiny setup -s253719

---
+++ server.R
@@ -0,0 +1,11 @@
+# Load needed libraries
+library("shiny")
+library("bslib")
+
+# Load needed functions
+source(file = "app_functions.R")
+
+# Define the Server (Backend)
+server <- function(input, output) {
+  
+}
---
+++ ui.R
@@ -0,0 +1,8 @@
+# Load needed libraries
+library("shiny")
+library("bslib")
+
+# Define the User Interface (Frontend)
+ui <- page_fluid(
+  
+)
