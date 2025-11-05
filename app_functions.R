dd58b8002ecd69e4afa8da8902ce8007ccc16873
quiquegomes123-cloud <quique.gomes123@gmail.com>
2025-11-05 16:41:13 +0100

    s253710, adding the transcribe_dna function

---
+++ app_functions.R
@@ -0,0 +1,8 @@
+# Virtual RNA polymerase
+transcribe_dna <- function(dna){
+  rna <- gsub(
+    pattern = "T",
+    replacement = "U",
+    x = dna)
+  return(rna)
+}
--- server.R
+++ server.R
@@ -7,6 +7,7 @@ source(file = "app_functions.R")
 
 # Define the Server (Backend)
 server <- function(input, output) {
-  
-
+  output$rna <- renderText({
+    transcribe_dna(dna = input$DNA_seq)
+  })
 }
