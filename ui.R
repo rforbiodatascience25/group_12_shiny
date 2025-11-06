dd1be55fd7ecacf83592d0ea5d957e763d2eaa60
paulameerjanssen <s253689@dtu.dk>
2025-11-06 11:16:08 +0100

    change to ui to make output look nicer

--- ui.R
+++ ui.R
@@ -21,6 +21,7 @@ ui <- page_fluid(
     card(
       card_header("Transcription"),
       layout_columns(
+        col_widths = 12,
         textInput(inputId = "DNA_seq",
                   label = "Insert a DNA sequence to transcribe:",
                   value = ""),
@@ -32,10 +33,13 @@ ui <- page_fluid(
     col_widths = 12,
     card(
       card_header("Translation"),
-      textInput(inputId = "RNA_seq", 
-                label = "Insert RNA sequence to translate:", 
-                value = ""),
-      verbatimTextOutput("protein")
+      layout_columns(
+        col_widths = 12,
+        textInput(inputId = "RNA_seq", 
+                  label = "Insert RNA sequence to translate:", 
+                  value = ""),
+        verbatimTextOutput("protein")
+      )
       )
     )
   )
