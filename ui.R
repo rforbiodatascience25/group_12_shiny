5ee7e6f943cc02e58307f02fe1382f30c07190f5
octopusply <sylviaz0805@gmail.com>
2025-11-06 15:42:32 +0100

    s252750: add description

--- ui.R
+++ ui.R
@@ -1,7 +1,3 @@
-# Load needed libraries
-library("shiny")
-library("bslib")
-
 # Define the User Interface (Frontend)
 ui <- page_fluid(
   
@@ -14,6 +10,31 @@ ui <- page_fluid(
       titlePanel("Virtual Central Dogma"),
       style = "background-color: #f0f0f0; padding: 15px;"
       )),
+  layout_columns(
+    col_widths = 12,
+    card(
+      titlePanel("About"),
+      helpText(HTML(
+        "Welcome to the Virtual Central Dogma App! 🧬<br><br>
+
+This interactive app allows you to explore the central dogma of molecular biology — from DNA to RNA to protein — in a virtual and intuitive way. Here's what you can do:<br>
+
+1. Virtual Gene Generation<br>
+   - Use the slider to select the number of DNA bases for your virtual gene (must be divisible by 3). <br>
+   - Adjust the probabilities of each nucleotide (A, T, C, G) to generate custom DNA sequences.<br><br>
+
+2. Transcription Simulation<br>
+   - The app automatically transcribes your virtual DNA into RNA, showing the corresponding transcript.<br><br>
+
+3. Translation Simulation<br>
+   - The RNA is then translated into a protein sequence based on codon mapping.<br><br>
+
+4. Base Count Analysis <br>
+   - Easily inspect the frequency of each base (A, T, C, G) in your DNA sequence with a simple summary table.<br><br>
+
+Tip: Make sure your nucleotide probabilities sum to 1. If not, the app will normalize them automatically."
+      ))
+    )),
   
   #Virtual RNA Polymerase
   layout_columns(
